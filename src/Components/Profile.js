684c9cedf5ca54413e91795a90ac34569a033ac3
AidanMulaokar <aidanpm123@gmail.com>
2020-04-02 04:09:35 -0400

    allow user to change profile details

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -8,10 +8,15 @@ class Post extends React.Component {
         this.state = {
             user: this.props.user,
             image: null,
-            url: ''
+            url: '',
+            disabled: true,
+            username: null,
+            name: null,
+            major: null
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleUpload = this.handleUpload.bind(this);
+        this.edit = this.edit.bind(this);
     }
 
     static getDerivedStateFromProps(props, state) {
@@ -19,13 +24,21 @@ class Post extends React.Component {
             console.log(props.user.id);
             return {
                     user: props.user,
-                    url: props.user.profileURL
+                    url: props.user.profileURL,
+                    username: props.user.username,
+                    name: props.user.name,
+                    major: props.user.major,
+                    disabled: true
             };
         } else if (props.user && !state.user){
             console.log(props.user.id);
             return {
                 user: props.user,
-                url: props.user.profileURL
+                url: props.user.profileURL,
+                username: props.user.username,
+                name: props.user.name,
+                major: props.user.major,
+                disabled: true
             };
         }
         return null;
@@ -62,19 +75,74 @@ class Post extends React.Component {
 
     }
 
+    setUsername(event) {
+        this.setState({username: event.target.value});
+    }
+
+    setName(event) {
+        this.setState({name: event.target.value});
+    }
+
+    setMajor(event) {
+        this.setState({major: event.target.value});
+    }
+
+    editUser(event) {
+        event.preventDefault();
+        firestore.collection('students').doc(this.state.user.id).update({
+            username: this.state.username,
+            name: this.state.name,
+            major: this.state.major,
+        });
+    }
+
     render() {
         return(
             <div className = "profile" id="Profile">
               <input type="file" onChange={this.handleChange}/>
                 <button onClick={this.handleUpload}>Upload Image</button>
                 <img src = {this.state.url} alt="Profile Pic" height="300" width="400"/>
+                <button onClick={this.edit}>Edit</button>
+                <form id="profileForm" onSubmit={e=>this.editUser(e)}>
+                    <label>Username</label>
+                    <input id="profileElement1" defaultValue={this.state.username} onChange= {(e)=>this.setUsername(e)} disabled></input>
+
+                    <label>Name</label>
+                    <input id="profileElement2"  defaultValue={this.state.name} onChange= {(e)=>this.setName(e)} disabled></input>
+
+                    <label>Major</label>
+                    <input id="profileElement3" defaultValue={this.state.major} onChange= {(e)=>this.setMajor(e)} disabled></input>
+
+                    <button type="submit">Submit</button>
+                </form>
                 <button onClick={this.closeProfile}>Close</button>
             </div>
+            
         );
     }
 
+    edit() {
+        console.log("edit");
+        if(document.getElementById("profileElement1").disabled) {
+            document.getElementById("profileElement1").disabled = false;
+            document.getElementById("profileElement2").disabled = false;
+            document.getElementById("profileElement3").disabled = false;
+            //this.state.disabled = false;
+            //console.log(document.getElementById("profileElement1").disabled);
+        } else {
+            document.getElementById("profileElement1").disabled = true;
+            document.getElementById("profileElement2").disabled = true;
+            document.getElementById("profileElement3").disabled = true;
+            //this.state.disabled = true;
+            //console.log(document.getElementById("profileElement1").disabled);
+        }
+    }
     closeProfile() {
+        console.log("closeProfile");
         document.getElementById("Profile").style.display = "none";
+        document.getElementById("profileElement1").disabled = true;
+        document.getElementById("profileElement2").disabled = true;
+        document.getElementById("profileElement3").disabled = true;
     }
 }
 export default Post;
